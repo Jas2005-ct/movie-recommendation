dbf58f35b34b57face23e4fb5d03c3000049bb1b
Jaswant Mayilsamy <jaswantjas02@gmail.com>
2026-03-31 22:19:18 +0530

    feat: template-based architecture

--- movies/settings.py
+++ movies/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'movies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template')],
+        'DIRS': [os.path.join(BASE_DIR, 'Template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
